02f77e4a30096c16ea460fbdac173752a466e01d
salmanfarizs <75155845+salmanfarizs@users.noreply.github.com>
2020-12-11 20:40:33 +0530

    Update Login.java

--- BuildAutomation/src/main/java/chrome/Login.java
+++ BuildAutomation/src/main/java/chrome/Login.java
@@ -13,7 +13,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Login {
 
 	RemoteWebDriver driver;
-
+	//salman comment
 	@Test
 	public void loginToLeafTaps() {
                  // Read the param                             
